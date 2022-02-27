56b4c35b7abe98769e25c5cccc2f26d2961c075e
Bernd Porr <mail@berndporr.me.uk>
2022-02-27 17:17:32 +0000

    Added script to calc jaw clench

---
+++ eeg_filter/run_jaw.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Jaw clench experiment data:
+# Filters all subjects without graphical output
+
+mkdir -p jawclench
+n=1;
+max=20;
+while [ "$n" -le "$max" ]; do
+  rm -rf "jawclench/subject$n"
+  mkdir -p "jawclench/subject$n"
+  n=`expr "$n" + 1`;
+done
+
+echo "Successfully created the results folders"
+
+./eeg_filter -b jawclench
