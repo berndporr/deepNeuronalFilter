3524358c75660c2152d988a03cdc1de459bdd6e7
Bernd Porr <mail@berndporr.me.uk>
2022-08-26 14:17:01 +0100

    Added info about this subdir

---
+++ eeg_sim/noise_psd/README.md
@@ -0,0 +1,29 @@
+# Power spectra during jaw clench task
+
+The aim is to assess the power spectrum of facial EMG.
+The subject was asked to clench their jaw for approximately
+one second every 15secs.
+
+## Full recording
+
+Run `python ./plot_subj10_t_f.py`:
+
+![alt tag](wholerec.png)
+
+
+## Spectrum of the jaw clenches (EMG)
+
+All jaw clenches concatenated. Run: `python3 ./plot_subj10_t_f.py jawclench`:
+
+![alt tag](jawclench.png)
+
+
+## Spectrum at rest
+
+Run `python3 ./plot_subj10_t_f.py rest`:
+
+![alt tag](rest.png)
+
+
+## How to run
+
--- eeg_sim/noise_psd/plot_subj10_t_f.py
+++ eeg_sim/noise_psd/plot_subj10_t_f.py
@@ -34,28 +34,20 @@ class tasks_eeg:
         
         self.t = np.linspace(0,len(self.data)/self.Fs,len(self.data))
         self.ch1 = self.data[:,7]
-        self.ch2 = self.data[:,8]
-        if not filterData:
-            return
 
         # Remove DC
-        bHigh,aHigh = signal.butter(4,1/self.Fs*2,'high')
+        bHigh,aHigh = signal.butter(4,0.2/self.Fs*2,'high')
         self.ch1 = signal.lfilter(bHigh,aHigh,self.ch1);
-        self.ch2 = signal.lfilter(bHigh,aHigh,self.ch2);
         
         # Remove 50Hz noise
         b50,a50 = signal.butter(4,[48/self.Fs*2,52/self.Fs*2],'stop')
         self.ch1 = signal.lfilter(b50,a50,self.ch1);
-        self.ch2 = signal.lfilter(b50,a50,self.ch2);
         
         # Remove 150Hz interference
         b150,a150 = signal.butter(4,[148/self.Fs*2,152/self.Fs*2],'stop')
         self.ch1 = signal.lfilter(b150,a150,self.ch1);
-        self.ch2 = signal.lfilter(b150,a150,self.ch2);
 
-        # 2secs muted
-        self.ch1[0:1000] = 0
-        self.ch2[0:1000] = 0
+        self.ch1 = self.ch1[self.Fs*20:]
 
 
 # check if we run this as a main program
@@ -64,48 +56,55 @@ if __name__ == "__main__":
     subj = 10
     toplot = tasks_eeg(subj,task)
 
+    jawclenchIntervals = [
+        [ 9.7, 10.6 ],
+        [ 25.0, 25.9 ],
+        [ 40.0, 40.5 ],
+        [ 55.2, 55.9 ],
+        [ 69.7, 70.6 ],
+        [ 85.0, 86.0 ],
+        [ 99.7, 100.6 ]
+    ]
+
+    ch1 = toplot.ch1
+
     a = 0
     b = -1
+    title = "Full EEG recording during repetitve jaw clench"
 
     if len(sys.argv) < 2:
         print("Specify 'rest' or 'jawclench' to plot the spectrum at rest or during a jaw clench");
-        quit()
     else:
         if sys.argv[1] == 'jawclench':
-            a = int(toplot.Fs * 28.6)
-            b = int(toplot.Fs * 30.6)
+            ch1 = np.empty(0)
+            for i in jawclenchIntervals:
+                a = int(toplot.Fs * i[0])
+                b = int(toplot.Fs * i[1])
+                print("Using jaw clench: {}sec - {}sec".format(i[0],i[1]))
+                ch1 = np.append(ch1,toplot.ch1[a:b])
+            title = "Jawclench segments"
             
         if sys.argv[1] == 'rest':
-            a = int(toplot.Fs * 18)
-            b = int(toplot.Fs * 28)
+            print("Using data from 18sec - 28sec")
+            a = int(toplot.Fs * 42)
+            b = int(toplot.Fs * 53)
+            ch1 = toplot.ch1[a:b]
+            title = "EEG at rest"
 
-    plt.figure('Time domain')
-    plt.suptitle(task+', Participant '+str(subj))
+    t = np.linspace(0,len(ch1)/toplot.Fs,len(ch1))
+
+    plt.figure()
+    plt.suptitle('Participant '+str(subj)+' '+title)
     plt.subplot(211)
-    plt.plot(toplot.t,toplot.ch1)
-    plt.title('Channel 1')
+    plt.plot(t,ch1 * 1E6)
     plt.xlabel('Time (sec)')
-    plt.ylabel('Amplitude (V)')
+    plt.ylabel('Amplitude (uV)')
+    plt.ylim([-400,400])
     plt.subplot(212)
-    plt.plot(toplot.t,toplot.ch2)
-    plt.title('Channel 2')
-    plt.xlabel('Time (sec)')
-    plt.ylabel('Amplitude (V)')
 
-    f1, Pxx_den1 = signal.periodogram(toplot.ch1[a:b],toplot.Fs,scaling='spectrum')
-    f2, Pxx_den2 = signal.periodogram(toplot.ch2[a:b],toplot.Fs,scaling='spectrum')
-    plt.figure("Freq domain")
-    plt.suptitle(task+', Participant '+str(subj))
-    plt.subplot(211)
-    plt.plot(f1, np.log10(np.abs(Pxx_den1)))
-    plt.title('Channel 1')
+    f, Pxx_den = signal.periodogram(ch1,toplot.Fs,scaling='spectrum')
+    plt.plot(f, np.log10(np.abs(Pxx_den)))
     plt.xlabel('Freq/Hz')
     plt.ylabel('Log Power (V^2/Hz)')
-    plt.ylim([-16,-10])
-    plt.subplot(212)
-    plt.plot(f2, np.log10(np.abs(Pxx_den2)))
-    plt.title('Channel 2')
-    plt.xlabel('Freq/Hz')
-    plt.ylabel('Log Power (V^2/Hz)')
-    plt.ylim([-16,-10])
+    plt.ylim([-18,-9])
     plt.show()
