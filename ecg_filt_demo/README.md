d266ac6debc3287df638a41248994996dbab176e
Bernd Porr <mail@berndporr.me.uk>
2025-05-12 18:16:28 +0100

    Better log outputs.

--- ecg_filt_demo/README.md
+++ ecg_filt_demo/README.md
@@ -1,11 +1,11 @@
 # 50Hz filter
 
-Demo which filters about 50Hz from a noisy ECG.
+Demo which filters out 50Hz mains from a noisy ECG.
 This is a minimal example where the reference noise is just
 a 50Hz sine wave and the DNF removes the 50Hz from the
-ECG by using this 50Hz sine. This, of course, can also be
-done by a simple LMS FIR filter but this here serves as an example
-code which can then be used for more complex reference signals.
+ECG by using this 50Hz sine wave. This, of course, can also be
+done by a simple LMS FIR filter but it serves here as an example
+which can then be used for more complex reference signals.
 
 ## How to run
 
@@ -20,8 +20,8 @@ python plot_results.py
 ```
 
 which plots the contents of `ecg_filtered.dat` which has
-three columns: DNF output, input, noise reference. By default
+three columns: `DNF output, input, noise reference`. By default
 the script plots the output of the filter but you can specify
-as an argument the column.
+as an argument another column.
 
 ![alt tag](result.png)
--- ecg_filt_demo/ecg_filt_demo.cpp
+++ ecg_filt_demo/ecg_filt_demo.cpp
@@ -1,6 +1,9 @@
+/**
+ * Filter demo which removes 50Hz from an ECG containing
+ * 50Hz noise.
+ **/
 
 #include <chrono>
-
 #include <iostream>
 #include <stdio.h>
 #include <thread>
@@ -13,7 +16,8 @@
 
 using namespace std; 
 
-//The nlayers should be an integer of the total number of hidden layers required not including the input layer
+// Integer of the total number of hidden layers
+// required not including the input layer
 const int NLAYERS = 5;
 
 // Number of taps of the delay line
@@ -31,14 +35,17 @@ const Neuron::actMethod ACTIVATION = Neuron::Act_Tanh;
 // dnf learning rate
 const double dnf_learning_rate = 0.1;
 
+// input filename
+const char inputFilename[] = "ecg50hz.dat";
+
+// output filename
+const char outputFilename[] = "ecg_filtered.dat";
+
 int main(int argc, char* argv[]){
+    fprintf(stderr, "Reading noisy ECG file: %s.\n",inputFilename);
 
-    std::cout<<"Made it to the Start :)\n\n";
-
-    //Opening the .dat file and the output file
-    //in the final program this should be replaced with the mic inputs
-    FILE *finput = fopen("ecg50hz.dat","rt");
-    FILE *foutput = fopen("ecg_filtered.dat","wt");
+    FILE *finput = fopen(inputFilename,"rt");
+    FILE *foutput = fopen(outputFilename,"wt");
 
     double norm_noise_f = noise_f / fs;
     int nSamples = 0;
@@ -77,9 +84,8 @@ int main(int argc, char* argv[]){
 
     printf("Time taken = %f s, max sampling rate = %f Hz\n", seconds_taken, maxSamplingRate);
 
+    fprintf(stderr, "Written result to: %s.\n",outputFilename);
+
     fclose(finput);
     fclose(foutput);
-
-    std::cout<<"Made it to the End :)\n\n\n";
-    
 }
